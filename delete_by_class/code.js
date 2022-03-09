c11dcc6d79bbaf00bc1f79e587e963567343fdba
blackarmVR <honungsbjorn@protonmail.com>
2022-03-09 15:33:36 +0000

    :)

--- delete_by_class/code.js
+++ delete_by_class/code.js
@@ -1,11 +1,14 @@
 "use strict";
-var deleteByClass = function (targetClass) {
+var deleteByClass = function () {
     var deleteThese = [];
-    deleteThese = Array.prototype.slice.call(document.getElementsByClassName(targetClass));
+    deleteThese = Array.prototype.slice.call(document.getElementsByClassName('delete'));
     deleteThese.forEach(function (target) {
         if (target.parentNode !== null) {
             target.parentNode.removeChild(target);
         }
     });
 };
-deleteByClass('delete');
+var doStuffButton = document.getElementById('doStuff');
+if (doStuffButton !== null) {
+    doStuffButton.addEventListener('click', deleteByClass);
+}
--- delete_by_class/code.ts
+++ delete_by_class/code.ts
@@ -1,10 +1,13 @@
-const deleteByClass = (targetClass: string) => {
+const deleteByClass = () => {
   let deleteThese: Array<HTMLElement> = [];
-  deleteThese = Array.prototype.slice.call(document.getElementsByClassName(targetClass));
+  deleteThese = Array.prototype.slice.call(document.getElementsByClassName('delete'));
   deleteThese.forEach((target: HTMLElement) => {
     if (target.parentNode !== null) {
       target.parentNode.removeChild(target);
     }
   });
 };
-deleteByClass('delete');
+let doStuffButton: HTMLButtonElement | null = <HTMLButtonElement>document.getElementById('doStuff');
+if (doStuffButton !== null) {
+  doStuffButton.addEventListener('click', deleteByClass);
+}
