9c75269f838c458481b986aa8407de98bb005d09
blackarmVR <honungsbjorn@protonmail.com>
2022-03-09 15:52:05 +0000

    :)

--- add_html_table/code.js
+++ add_html_table/code.js
@@ -1,3 +1,4 @@
+"use strict";
 function createTable(divName, dataHeader, dataContent) {
     var cell;
     var child;
@@ -20,19 +21,19 @@ function createTable(divName, dataHeader, dataContent) {
         row = thead.insertRow();
         for (var _i = 0, dataHeader_1 = dataHeader; _i < dataHeader_1.length; _i++) {
             var header = dataHeader_1[_i];
-            th = document.createElement("th");
+            th = document.createElement('th');
             text = document.createTextNode(header);
             th.appendChild(text);
             row.appendChild(th);
         }
-        tbody = document.createElement("tbody");
+        tbody = document.createElement('tbody');
         for (var _a = 0, dataContent_1 = dataContent; _a < dataContent_1.length; _a++) {
             var rowContent = dataContent_1[_a];
             row = tbody.insertRow();
             for (var _b = 0, rowContent_1 = rowContent; _b < rowContent_1.length; _b++) {
                 var cellData = rowContent_1[_b];
                 cell = row.insertCell();
-                cell.style.backgroundColor = "rgb(255,229.5,127.5)";
+                cell.style.backgroundColor = 'rgb(255,229.5,127.5)';
                 text = document.createTextNode(cellData);
                 cell.appendChild(text);
                 row.appendChild(cell);
@@ -42,9 +43,9 @@ function createTable(divName, dataHeader, dataContent) {
         div.appendChild(table);
     }
 }
-var dataHeader = ["header1", "header2", "header3"];
+var dataHeader = ['header1', 'header2', 'header3'];
 var dataContent = [
-    ["alpha1", "bravo1", "charlie1"],
-    ["alpha2", "bravo2", "charlie1"]
+    ['alpha1', 'bravo1', 'charlie1'],
+    ['alpha2', 'bravo2', 'charlie1'],
 ];
-createTable("target", dataHeader, dataContent);
+createTable('target', dataHeader, dataContent);
