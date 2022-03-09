5f50b30af23e364add4c812b84d1eba4a502ae76
blackarmVR <honungsbjorn@protonmail.com>
2022-03-09 15:05:39 +0000

    :)

--- change_elements_by_class/code.js
+++ change_elements_by_class/code.js
@@ -1,8 +1,11 @@
 "use strict";
-var changeElement;
-var elementArray = [];
-elementArray = Array.prototype.slice.call(document.getElementsByClassName('change'));
-for (var _i = 0, elementArray_1 = elementArray; _i < elementArray_1.length; _i++) {
-    changeElement = elementArray_1[_i];
-    changeElement.style.fontSize = '100px';
-}
+var changeByClass = function (targetClass) {
+    var changeElement;
+    var elementArray = [];
+    elementArray = Array.prototype.slice.call(document.getElementsByClassName(targetClass));
+    for (var _i = 0, elementArray_1 = elementArray; _i < elementArray_1.length; _i++) {
+        changeElement = elementArray_1[_i];
+        changeElement.style.fontSize = '100px';
+    }
+};
+changeByClass('change');
--- change_elements_by_class/code.ts
+++ change_elements_by_class/code.ts
@@ -1,6 +1,9 @@
-let changeElement: HTMLElement;
-let elementArray: Array<HTMLElement> = [];
-elementArray = Array.prototype.slice.call(document.getElementsByClassName('change'));
-for (changeElement of elementArray){
-  changeElement.style.fontSize = '100px';
-}
+const changeByClass = (targetClass: string) => {
+  let changeElement: HTMLElement;
+  let elementArray: Array<HTMLElement> = [];
+  elementArray = Array.prototype.slice.call(document.getElementsByClassName(targetClass));
+  for (changeElement of elementArray){
+    changeElement.style.fontSize = '100px';
+  }
+};
+changeByClass('change');
