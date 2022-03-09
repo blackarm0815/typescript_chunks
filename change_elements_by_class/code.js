3e598d430c30de0e9498961572abb9372a31e9d6
blackarmVR <honungsbjorn@protonmail.com>
2022-03-09 15:31:58 +0000

    :)

--- change_elements_by_class/code.js
+++ change_elements_by_class/code.js
@@ -1,11 +1,14 @@
 "use strict";
-var changeByClass = function (targetClass) {
+var changeByClass = function () {
     var changeElement;
     var elementArray = [];
-    elementArray = Array.prototype.slice.call(document.getElementsByClassName(targetClass));
+    elementArray = Array.prototype.slice.call(document.getElementsByClassName('change'));
     for (var _i = 0, elementArray_1 = elementArray; _i < elementArray_1.length; _i++) {
         changeElement = elementArray_1[_i];
-        changeElement.style.fontSize = '100px';
+        changeElement.style.fontSize = '50px';
     }
 };
-changeByClass('change');
+var doStuffButton = document.getElementById('doStuff');
+if (doStuffButton !== null) {
+    doStuffButton.addEventListener('click', changeByClass);
+}
--- change_elements_by_class/code.ts
+++ change_elements_by_class/code.ts
@@ -1,9 +1,12 @@
-const changeByClass = (targetClass: string) => {
+const changeByClass = () => {
   let changeElement: HTMLElement;
   let elementArray: Array<HTMLElement> = [];
-  elementArray = Array.prototype.slice.call(document.getElementsByClassName(targetClass));
+  elementArray = Array.prototype.slice.call(document.getElementsByClassName('change'));
   for (changeElement of elementArray){
-    changeElement.style.fontSize = '100px';
+    changeElement.style.fontSize = '50px';
   }
 };
-changeByClass('change');
+let doStuffButton: HTMLButtonElement | null = <HTMLButtonElement>document.getElementById('doStuff');
+if (doStuffButton !== null) {
+  doStuffButton.addEventListener('click', changeByClass);
+}
