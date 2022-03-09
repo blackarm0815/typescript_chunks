6453347b11390fe332344e0835733e3c0bad2d11
blackarmVR <honungsbjorn@protonmail.com>
2022-03-09 14:25:41 +0000

    :)

---
+++ add_html_button/code.ts
@@ -0,0 +1,13 @@
+var output: HTMLElement | null;
+var button: HTMLInputElement;
+output = document.getElementById('output');
+if (output !== null) {
+  button = document.createElement('input');
+  button.setAttribute('type', 'button');
+  button.setAttribute('value', 'Click me');
+  button.setAttribute('name', 'buttonId');
+  output.appendChild(button);
+  button.addEventListener('click', () => {
+    console.log('click');
+  }, false);
+}
