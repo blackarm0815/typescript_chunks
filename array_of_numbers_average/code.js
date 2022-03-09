36b68e5401b39f171d4f978d88d3825d3f182fc4
blackarmVR <honungsbjorn@protonmail.com>
2022-03-09 13:43:24 +0000

    :)

--- array_of_numbers_average/code.js
+++ array_of_numbers_average/code.js
@@ -1,24 +1,25 @@
+"use strict";
 var people = [
     {
         name: 'Sarah',
         gender: 'female',
-        age: 25
+        age: 25,
     },
     {
         name: 'Tom',
         gender: 'male',
-        age: 18
+        age: 18,
     },
     {
         name: 'Tim',
         gender: 'male',
-        age: 65
+        age: 65,
     },
     {
         name: 'Kim',
         gender: 'female',
-        age: 58
-    }
+        age: 58,
+    },
 ];
 var average = people.reduce(function (total, next) { return total + next.age; }, 0) / people.length;
 console.log(average);
---
+++ array_of_numbers_average/code.ts
@@ -0,0 +1,30 @@
+interface Person {
+  name: string;
+  gender: string;
+  age: number;
+}
+
+const people: Array<Person> = [
+  {
+    name:'Sarah',
+    gender:'female',
+    age:25,
+  },
+  {
+    name:'Tom',
+    gender:'male',
+    age:18,
+  },
+  {
+    name:'Tim',
+    gender:'male',
+    age:65,
+  },
+  {
+    name:'Kim',
+    gender:'female',
+    age:58,
+  },
+];
+const average: number = people.reduce((total, next) => total + next.age, 0) / people.length;
+console.log(average);
