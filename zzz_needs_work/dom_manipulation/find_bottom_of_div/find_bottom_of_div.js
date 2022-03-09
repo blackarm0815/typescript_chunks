b179430cfcfc3f1d845505781fbcf648fa3ca93e
blackarmVR <honungsbjorn@protonmail.com>
2022-03-09 17:52:29 +0000

    :)

--- zzz_needs_work/dom_manipulation/find_bottom_of_div/find_bottom_of_div.js
+++
@@ -1,7 +0,0 @@
-var divElement;
-var topDivUnder;
-divElement = document.getElementById('top');
-topDivUnder = 0;
-if (divElement) {
-    topDivUnder = divElement.offsetTop + divElement.getBoundingClientRect().height;
-}
--- zzz_needs_work/dom_manipulation/find_bottom_of_div/find_bottom_of_div.ts
+++
@@ -1,8 +0,0 @@
-
-let divElement: HTMLElement | null;
-let topDivUnder: number;
-divElement = <HTMLElement>document.getElementById('top');
-topDivUnder = 0;
-if (divElement) {
-	topDivUnder = divElement.offsetTop + divElement.getBoundingClientRect().height;
-}
