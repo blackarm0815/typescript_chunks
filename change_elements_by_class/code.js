624a69bca1fb8ae9e166762486a1b583bf2d1f42
blackarm0815 <honungsbjorn@protonmail.com>
2022-08-23 18:10:00 +0100

    lazy commit

--- change_elements_by_class/code.js
+++ change_elements_by_class/code.js
@@ -1,12 +1,10 @@
 "use strict";
 var changeByClass = function () {
-    var changeElement;
     var elementArray = [];
     elementArray = Array.prototype.slice.call(document.getElementsByClassName('change'));
-    for (var _i = 0, elementArray_1 = elementArray; _i < elementArray_1.length; _i++) {
-        changeElement = elementArray_1[_i];
+    elementArray.forEach(function (changeElement) {
         changeElement.style.fontSize = '50px';
-    }
+    });
 };
 var doStuffButton = document.getElementById('doStuff');
 if (doStuffButton !== null) {
--- change_elements_by_class/code.ts
+++ change_elements_by_class/code.ts
@@ -1,10 +1,9 @@
 const changeByClass = () => {
-  let changeElement: HTMLElement;
   let elementArray: Array<HTMLElement> = [];
   elementArray = Array.prototype.slice.call(document.getElementsByClassName('change'));
-  for (changeElement of elementArray){
+  elementArray.forEach((changeElement) => {
     changeElement.style.fontSize = '50px';
-  }
+  });
 };
 let doStuffButton: HTMLButtonElement | null = <HTMLButtonElement>document.getElementById('doStuff');
 if (doStuffButton !== null) {
