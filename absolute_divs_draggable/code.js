8a0abdd034c8cf6ed75bd3c4a494407a252b0228
blackarm0815 <honungsbjorn@protonmail.com>
2023-03-15 16:04:48 +0000

    fixed

--- absolute_divs_draggable/code.js
+++ absolute_divs_draggable/code.js
@@ -5,42 +5,38 @@ var pageDivDimensions = function () {
     var border = 5;
     var divElement;
     var lineThickness = 3;
-    var pageHeight;
-    var pageWidth;
-    var topElement;
     var topDivUnder;
-    var rect;
-    pageWidth = window.innerWidth;
-    pageHeight = window.innerHeight;
-    topElement = document.getElementById('top');
+    var pageWidth = window.innerWidth;
+    var pageHeight = window.innerHeight;
+    var topElement = document.getElementById('top');
     topDivUnder = 0;
     if (topElement) {
-        rect = topElement.getBoundingClientRect();
+        var rect = topElement.getBoundingClientRect();
         topDivUnder = rect.top + rect.height + border;
     }
     // alpha
     divElement = document.getElementById('alpha');
     if (divElement) {
-        divElement.style.left = border + 'px';
-        divElement.style.top = topDivUnder + 'px';
-        divElement.style.width = (dragHorizontalPosition - (lineThickness * 0.5) - (border * 2)) + 'px';
-        divElement.style.height = (pageHeight - topDivUnder - (border * 2)) + 'px';
+        divElement.style.left = "".concat(border, "px");
+        divElement.style.top = "".concat(topDivUnder, "px");
+        divElement.style.width = "".concat((dragHorizontalPosition - (lineThickness * 0.5) - (border * 2)), "px");
+        divElement.style.height = "".concat((pageHeight - topDivUnder - (border * 2)), "px");
     }
     // bravo
     divElement = document.getElementById('bravo');
     if (divElement) {
-        divElement.style.left = (dragHorizontalPosition + border + (lineThickness * 0.5)) + 'px';
-        divElement.style.top = topDivUnder + 'px';
-        divElement.style.width = (pageWidth - dragHorizontalPosition - (border * 2) - (lineThickness * 0.5)) + 'px';
-        divElement.style.height = (pageHeight - topDivUnder - (border * 2)) + 'px';
+        divElement.style.left = "".concat((dragHorizontalPosition + border + (lineThickness * 0.5)), "px");
+        divElement.style.top = "".concat(topDivUnder, "px");
+        divElement.style.width = "".concat((pageWidth - dragHorizontalPosition - (border * 2) - (lineThickness * 0.5)), "px");
+        divElement.style.height = "".concat((pageHeight - topDivUnder - (border * 2)), "px");
     }
     // horizontalDrag
     divElement = document.getElementById('horizontalDrag');
     if (divElement) {
-        divElement.style.left = (dragHorizontalPosition - (lineThickness * 0.5)) + 'px';
-        divElement.style.top = topDivUnder + 'px';
-        divElement.style.width = lineThickness + 'px';
-        divElement.style.height = (pageHeight - topDivUnder - (border * 2)) + 'px';
+        divElement.style.left = "".concat((dragHorizontalPosition - (lineThickness * 0.5)), "px");
+        divElement.style.top = "".concat(topDivUnder, "px");
+        divElement.style.width = "".concat(lineThickness, "px");
+        divElement.style.height = "".concat((pageHeight - topDivUnder - (border * 2)), "px");
     }
 };
 var repositionDrag = function (mouseEvent) {
@@ -48,8 +44,7 @@ var repositionDrag = function (mouseEvent) {
     pageDivDimensions();
 };
 var dragSetup = function () {
-    var dragHorizontal;
-    dragHorizontal = document.getElementById('horizontalDrag');
+    var dragHorizontal = document.getElementById('horizontalDrag');
     if (dragHorizontal) {
         dragHorizontal.addEventListener('mousedown', function () {
             document.addEventListener('mousemove', repositionDrag, false);
--- absolute_divs_draggable/code.ts
+++ absolute_divs_draggable/code.ts
@@ -1,45 +1,41 @@
 let dragHorizontalPosition = 200;
 //
 const pageDivDimensions = () => {
-  const border: number = 5;
+  const border = 5;
   let divElement: HTMLElement | null;
-  const lineThickness: number = 3;
-  let pageHeight: number;
-  let pageWidth: number;
-  let topElement: HTMLElement | null;
+  const lineThickness = 3;
   let topDivUnder: number;
-  var rect: DOMRect;
-  pageWidth = window.innerWidth;
-  pageHeight = window.innerHeight;
-  topElement = document.getElementById('top');
+  const pageWidth = window.innerWidth;
+  const pageHeight = window.innerHeight;
+  const topElement = document.getElementById('top');
   topDivUnder = 0;
   if (topElement) {
-    rect = topElement.getBoundingClientRect();
+    const rect = topElement.getBoundingClientRect();
     topDivUnder = rect.top + rect.height + border;
   }
   // alpha
   divElement = document.getElementById('alpha');
   if (divElement) {
-    divElement.style.left = border + 'px';
-    divElement.style.top = topDivUnder + 'px';
-    divElement.style.width = (dragHorizontalPosition  - (lineThickness * 0.5) - (border * 2)) + 'px';
-    divElement.style.height = (pageHeight - topDivUnder - (border * 2)) + 'px';
+    divElement.style.left = `${border}px`;
+    divElement.style.top = `${topDivUnder}px`;
+    divElement.style.width = `${(dragHorizontalPosition - (lineThickness * 0.5) - (border * 2))}px`;
+    divElement.style.height = `${(pageHeight - topDivUnder - (border * 2))}px`;
   }
   // bravo
   divElement = document.getElementById('bravo');
   if (divElement) {
-    divElement.style.left = (dragHorizontalPosition + border + (lineThickness * 0.5)) + 'px';
-    divElement.style.top = topDivUnder + 'px';
-    divElement.style.width = (pageWidth - dragHorizontalPosition - (border * 2) - (lineThickness * 0.5)) + 'px';
-    divElement.style.height = (pageHeight - topDivUnder - (border * 2)) + 'px';
+    divElement.style.left = `${(dragHorizontalPosition + border + (lineThickness * 0.5))}px`;
+    divElement.style.top = `${topDivUnder}px`;
+    divElement.style.width = `${(pageWidth - dragHorizontalPosition - (border * 2) - (lineThickness * 0.5))}px`;
+    divElement.style.height = `${(pageHeight - topDivUnder - (border * 2))}px`;
   }
   // horizontalDrag
   divElement = document.getElementById('horizontalDrag');
   if (divElement) {
-    divElement.style.left = (dragHorizontalPosition - (lineThickness * 0.5)) + 'px';
-    divElement.style.top = topDivUnder + 'px';
-    divElement.style.width = lineThickness + 'px';
-    divElement.style.height = (pageHeight - topDivUnder - (border * 2)) + 'px';
+    divElement.style.left = `${(dragHorizontalPosition - (lineThickness * 0.5))}px`;
+    divElement.style.top = `${topDivUnder}px`;
+    divElement.style.width = `${lineThickness}px`;
+    divElement.style.height = `${(pageHeight - topDivUnder - (border * 2))}px`;
   }
 };
 const repositionDrag = (
@@ -49,15 +45,14 @@ const repositionDrag = (
   pageDivDimensions();
 };
 const dragSetup = () => {
-  let dragHorizontal: HTMLElement | null;
-  dragHorizontal = document.getElementById('horizontalDrag');
+  const dragHorizontal = document.getElementById('horizontalDrag');
   if (dragHorizontal) {
     dragHorizontal.addEventListener('mousedown', () => {
       document.addEventListener('mousemove', repositionDrag, false);
     }, false);
   }
   // deactivating drag actions when mouse button is released
-  document.addEventListener('mouseup', function (){
+  document.addEventListener('mouseup', () => {
     document.removeEventListener('mousemove', repositionDrag, false);
   }, false);
 };
