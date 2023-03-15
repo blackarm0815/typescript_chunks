a88a124a227a85d0cd051415f2ccd4d48a0d1cf4
blackarm0815 <honungsbjorn@protonmail.com>
2023-03-15 14:31:34 +0000

    fix

--- absolute_divs/code.js
+++ absolute_divs/code.js
@@ -6,34 +6,34 @@ var pageDivDimensions = function () {
     // alpha
     divElement = document.getElementById('alpha');
     if (divElement) {
-        divElement.style.left = "".concat((pageWidth * 0.1), " px");
-        divElement.style.top = "".concat((pageHeight * 0.1), " px");
-        divElement.style.width = "".concat((pageWidth * 0.3), " px");
-        divElement.style.height = "".concat((pageHeight * 0.3), " px");
+        divElement.style.left = "".concat((pageWidth * 0.1), "px");
+        divElement.style.top = "".concat((pageHeight * 0.1), "px");
+        divElement.style.width = "".concat((pageWidth * 0.3), "px");
+        divElement.style.height = "".concat((pageHeight * 0.3), "px");
     }
     // bravo
     divElement = document.getElementById('bravo');
     if (divElement) {
-        divElement.style.left = "".concat((pageWidth * 0.6), " px");
-        divElement.style.top = "".concat((pageHeight * 0.1), " px");
-        divElement.style.width = "".concat((pageWidth * 0.3), " px");
-        divElement.style.height = "".concat((pageHeight * 0.3), " px");
+        divElement.style.left = "".concat((pageWidth * 0.6), "px");
+        divElement.style.top = "".concat((pageHeight * 0.1), "px");
+        divElement.style.width = "".concat((pageWidth * 0.3), "px");
+        divElement.style.height = "".concat((pageHeight * 0.3), "px");
     }
     // charlie
     divElement = document.getElementById('charlie');
     if (divElement) {
-        divElement.style.left = "".concat((pageWidth * 0.1), " px");
-        divElement.style.top = "".concat((pageHeight * 0.6), " px");
-        divElement.style.width = "".concat((pageWidth * 0.3), " px");
-        divElement.style.height = "".concat((pageHeight * 0.3), " px");
+        divElement.style.left = "".concat((pageWidth * 0.1), "px");
+        divElement.style.top = "".concat((pageHeight * 0.6), "px");
+        divElement.style.width = "".concat((pageWidth * 0.3), "px");
+        divElement.style.height = "".concat((pageHeight * 0.3), "px");
     }
     // delta
     divElement = document.getElementById('delta');
     if (divElement) {
-        divElement.style.left = "".concat((pageWidth * 0.6), " px");
-        divElement.style.top = "".concat((pageHeight * 0.6), " px");
-        divElement.style.width = "".concat((pageWidth * 0.3), " px");
-        divElement.style.height = "".concat((pageHeight * 0.3), " px");
+        divElement.style.left = "".concat((pageWidth * 0.6), "px");
+        divElement.style.top = "".concat((pageHeight * 0.6), "px");
+        divElement.style.width = "".concat((pageWidth * 0.3), "px");
+        divElement.style.height = "".concat((pageHeight * 0.3), "px");
     }
 };
 window.addEventListener('resize', pageDivDimensions);
